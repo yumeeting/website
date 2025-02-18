1553edb0d1d48acc8825be4397b684bfbdfa0afc
OnCloud <anonymousaaaa41414141@gmail.com>
2025-02-18 12:21:25 +0800

    feat: create main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
-import { Underline } from "@/components/Underline";
 import { Metadata } from "next";
 import Link from "next/link";
+import { Mic } from "lucide-react";
+
+import { Underline } from "@/components/Underline";
 
 export const metadata: Metadata = {
   title: "Home | YU Meeting",
@@ -9,10 +11,49 @@ export const metadata: Metadata = {
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-wrap justify-center items-center min-h-dvh">
-      <Link href="/login" className="fixed top-4 left-5">
-        <Underline>Login to use YU Meeting</Underline>
+    <>
+      <Link href="/login" className="fixed bottom-3 right-4">
+        <Underline>Go to login page</Underline>
       </Link>
-    </div>
+      <div className="flex flex-col flex-wrap justify-center items-center min-h-dvh">
+        <div className="w-11/12 max-w-96">
+          <div className="border border-foreground rounded-lg p-6 bg-gray-100">
+            <div>
+              <p className="text-lg font-bold font-outfit">YU Meeting</p>
+              <p className="text-xs font-inter mt-2">
+                copyright ©{" "}
+                <a
+                  href="https://yuniverses.com"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <span className="font-bold">YUNIVER</span>
+                </a>{" "}
+                ® {new Date().getFullYear()}
+              </p>
+            </div>
+            <button
+              type="button"
+              className="flex items-center justify-center mt-6 w-full h-12 rounded-full border-2 border-black text-neutral-800 bg-stone-50 hover:bg-stone-200 transition-colors duration-300"
+            >
+              <Mic />
+            </button>
+          </div>
+          <div className="bg-foreground h-11 rounded-md mt-4 flex items-center gap-4 px-7">
+            <p className="text-white cursor-pointer">
+              <Underline lineColor="bg-white">展開紀錄</Underline>
+            </p>
+            <div className="flex-grow" />
+            <div>
+              <input
+                type="text"
+                placeholder="搜索會議..."
+                className="bg-black text-white rounded-sm px-2 py-1 text-sm"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
