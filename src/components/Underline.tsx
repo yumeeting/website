6c3158a4510137f836017d4eae08e28d5503ac0d
OnCloud <anonymousaaaa41414141@gmail.com>
2025-02-18 12:21:10 +0800

    refactor: improve underline component

--- src/components/Underline.tsx
+++ src/components/Underline.tsx
@@ -1,8 +1,15 @@
-export function Underline({ children }: { children: React.ReactNode }) {
+export function Underline({
+  children,
+  lineColor,
+}: { children: React.ReactNode; lineColor?: string }) {
   return (
     <span className="relative group">
       {children}
-      <div className="absolute bottom-[0px] h-[1px] w-0 group-hover:w-full bg-black transition-all duration-200 ease-in-out" />
+      <div
+        className={`absolute bottom-[0px] h-[1px] w-0 group-hover:w-full transition-all duration-200 ease-in-out ${
+          lineColor ? lineColor : "bg-black"
+        }`}
+      />
     </span>
   );
 }
