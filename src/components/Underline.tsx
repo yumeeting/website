a486c0e154df1bf565090d8416f4a0f83622b65b
OnCloud <anonymousaaaa41414141@gmail.com>
2025-02-18 12:24:15 +0800

    fix: hydration error

--- src/components/Underline.tsx
+++ src/components/Underline.tsx
@@ -5,8 +5,8 @@ export function Underline({
   return (
     <span className="relative group">
       {children}
-      <div
-        className={`absolute bottom-[0px] h-[1px] w-0 group-hover:w-full transition-all duration-200 ease-in-out ${
+      <span
+        className={`absolute left-0 bottom-[0px] h-[1px] w-0 group-hover:w-full transition-all duration-200 ease-in-out ${
           lineColor ? lineColor : "bg-black"
         }`}
       />
