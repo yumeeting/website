6e91534d8766fcea8f15af9f1827c6b25fcb0979
OnCloud <anonymousaaaa41414141@gmail.com>
2025-02-18 21:45:44 +0800

    feat: add plus button at main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
+import { Mic, Plus } from "lucide-react";
 import { Metadata } from "next";
 import Link from "next/link";
-import { Mic } from "lucide-react";
 
 import { Underline } from "@/components/Underline";
 
@@ -32,14 +32,22 @@ export default function Home() {
                 ® {new Date().getFullYear()}
               </p>
             </div>
-            <button
-              type="button"
-              className="flex items-center justify-center mt-6 w-full h-12 rounded-full border-2 border-black text-neutral-800 bg-stone-50 hover:bg-stone-200 transition-colors duration-300"
-            >
-              <Mic />
-            </button>
+            <div className="flex gap-3 mt-6 w-full h-12">
+              <button
+                type="button"
+                className="flex-grow flex items-center justify-center rounded-full border-2 border-black text-neutral-800 bg-stone-50 hover:bg-stone-200 transition-colors duration-300"
+              >
+                <Mic />
+              </button>
+              <button
+                type="button"
+                className="flex items-center justify-center rounded-full hover:bg-stone-200 transition-colors duration-300 aspect-square"
+              >
+                <Plus className="h-3/5 w-auto" />
+              </button>
+            </div>
           </div>
-          <div className="bg-foreground h-11 rounded-md mt-4 flex items-center gap-4 px-7">
+          <div className="bg-foreground h-11 rounded-md mt-4 flex items-center gap-2 px-7">
             <p className="text-white cursor-pointer">
               <Underline lineColor="bg-white">展開紀錄</Underline>
             </p>
