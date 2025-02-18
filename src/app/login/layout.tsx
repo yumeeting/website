a732fcf49df79aa7a3b5cac689b20d869db068b6
OnCloud <anonymousaaaa41414141@gmail.com>
2025-02-19 00:14:16 +0800

    fear: implement login feature

---
+++ src/app/login/layout.tsx
@@ -0,0 +1,10 @@
+import { Metadata } from "next";
+
+import Page from "./page";
+
+export const metadata: Metadata = {
+  title: "Login | YU Meeting",
+  description: "Login to use YU Meeting",
+};
+
+export default Page;
