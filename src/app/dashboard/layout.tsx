399ce4b8f31e90205a4317d8ac55c1c1cc8d8bbb
OnCloud <anonymousaaaa41414141@gmail.com>
2025-02-20 00:15:09 +0800

    refactor: dashboard UI; add meeting modes, buttons, separators

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,4 +1,6 @@
 import { Metadata } from "next";
+import Image from "next/image";
+import Link from "next/link";
 
 import Page from "./page";
 
@@ -7,4 +9,26 @@ export const metadata: Metadata = {
   description: "捕捉每刻 靈感，會議記錄從未如此簡單",
 };
 
-export default Page;
+export default function _Page() {
+  return (
+    <>
+      <div className="fixed top-3 left-4">
+        <Link
+          href="/login"
+          className="flex items-center gap-2 hover:bg-stone-300 transition-colors duration-300 px-2 py-1 rounded-lg"
+        >
+          <Image
+            src="/images/logo.svg"
+            alt="YU Meeting Logo"
+            width={50}
+            height={50}
+            className="h-9 w-auto -mt-[2px]"
+            priority
+          />
+          <p className="text-lg font-semibold font-outfit">YU Meeting</p>
+        </Link>
+      </div>
+      <Page />
+    </>
+  );
+}
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,6 @@
 "use client";
 
 import { Frown, LogOut, Mic, Sparkle, Upload, User } from "lucide-react";
-import Link from "next/link";
-import Image from "next/image";
 import { permanentRedirect } from "next/navigation";
 import { useUser } from "@auth0/nextjs-auth0";
 import { useState } from "react";
@@ -26,8 +24,22 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { Separator } from "@/components/ui/separator";
+import Icons from "@/components/Icons";
+import { Button } from "@/components/ui/button";
 
 export default function Dashboard() {
+  const meetingModesOption = [
+    "一般會議記錄",
+    "一般會議記錄（非常詳細）",
+    "上課內容記錄",
+    "上課內容記錄（非常詳細）",
+    "專案會議記錄",
+    "通話記錄",
+    "合約會議記錄",
+  ];
+  const [meetingModeSelection, setMeetingModeSelection] = useState(0);
+
   const { user, isLoading } = useUser();
 
   const [isPro, setIsPro] = useState(false);
@@ -45,21 +57,76 @@ export default function Dashboard() {
 
   return (
     <>
-      <div className="fixed top-3 left-4">
-        <Link
-          href="/login"
-          className="flex items-center gap-2 hover:bg-stone-300 transition-colors duration-300 px-2 py-1 rounded-lg"
-        >
-          <Image
-            src="/images/logo.svg"
-            alt="YU Meeting Logo"
-            width={50}
-            height={50}
-            className="h-9 w-auto -mt-[2px]"
-            priority
-          />
-          <p className="text-lg font-semibold font-outfit">YU Meeting</p>
-        </Link>
+      <div className="flex flex-col flex-wrap justify-center items-center min-h-dvh">
+        <div className="w-11/12 max-w-md">
+          <div className="rounded-lg p-5 bg-white">
+            <div>
+              <p className="text-lg font-bold font-outfit">YU Meeting</p>
+              <p className="text-xs font-inter mt-2">
+                {"By proceeding, you agree to our "}
+                <span className="font-semibold underline">Terms of Use</span>
+                {"."}
+              </p>
+            </div>
+            <div className="flex sm:flex-row flex-col gap-3 mt-4 w-full">
+              <div className="flex-grow flex gap-3 h-12">
+                <button
+                  type="button"
+                  className="flex-grow flex items-center justify-center h-full rounded-full text-background bg-foreground hover:bg-stone-700 transition-colors duration-300"
+                >
+                  <Mic />
+                </button>
+                <button
+                  type="button"
+                  className="flex items-center justify-center aspect-square h-full rounded-full border border-foreground hover:bg-stone-200 transition-colors duration-300"
+                >
+                  <Upload className="h-5 w-auto" />
+                </button>
+              </div>
+              <Select>
+                <SelectTrigger className="sm:w-52 w-full h-12 bg-stone-100">
+                  <div>
+                    <p className="text-xs text-left text-stone-500">模式</p>
+                    <SelectValue
+                      placeholder={meetingModesOption[meetingModeSelection]}
+                    />
+                  </div>
+                </SelectTrigger>
+                <SelectContent>
+                  {meetingModesOption.map((mode, index) => (
+                    <SelectItem
+                      key={index}
+                      value={String(index)}
+                      className="cursor-pointer"
+                      onClick={() => setMeetingModeSelection(index)}
+                    >
+                      {mode}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+            </div>
+            <Separator className="mt-3" />
+            <Button className="mt-3 h-12 w-full bg-stone-100 hover:bg-stone-200">
+              <Icons type="discord" />
+              <Icons type="google-meet" />
+              <p className="-mt-[2px] text-foreground">使用會議軟體錄製</p>
+            </Button>
+          </div>
+          <div className="bg-foreground rounded-md mt-4 flex items-center gap-2 px-5 py-4">
+            <p className="text-white cursor-pointer">
+              <Underline lineColor="bg-white">展開紀錄</Underline>
+            </p>
+            <div className="flex-grow" />
+            <div>
+              <input
+                type="text"
+                placeholder="搜索會議..."
+                className="bg-black text-white rounded-sm px-2 py-1 text-sm"
+              />
+            </div>
+          </div>
+        </div>
       </div>
       <DropdownMenu>
         <DropdownMenuTrigger className="fixed top-3 right-4 flex items-center gap-2 rounded-lg border border-stone-300 bg-background px-3 py-2 max-w-56 min-w-48">
@@ -124,66 +191,6 @@ export default function Dashboard() {
           </a>
         </DropdownMenuContent>
       </DropdownMenu>
-      <div className="flex flex-col flex-wrap justify-center items-center min-h-dvh">
-        <div className="w-11/12 max-w-md">
-          <div className="rounded-lg p-5 bg-white">
-            <div>
-              <p className="text-lg font-bold font-outfit">YU Meeting</p>
-              <p className="text-xs font-inter mt-2">
-                {"By proceeding, you agree to our "}
-                <span className="font-semibold underline">Terms of Use</span>
-                {"."}
-              </p>
-            </div>
-            <div className="flex sm:flex-row flex-col gap-3 mt-4 w-full">
-              <div className="flex-grow flex gap-3 h-12">
-                <button
-                  type="button"
-                  className="flex-grow flex items-center justify-center h-full rounded-full text-background bg-foreground hover:bg-stone-700 transition-colors duration-300"
-                >
-                  <Mic />
-                </button>
-                <button
-                  type="button"
-                  className="flex items-center justify-center aspect-square h-full rounded-full border border-foreground hover:bg-stone-200 transition-colors duration-300"
-                >
-                  <Upload className="h-5 w-auto" />
-                </button>
-              </div>
-              <Select>
-                <SelectTrigger className="sm:w-52 w-full h-12 bg-stone-100">
-                  <div>
-                    <p className="text-xs text-left text-stone-500">模式</p>
-                    <SelectValue placeholder="一般會議記錄" />
-                  </div>
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectItem value="0">一般會議記錄</SelectItem>
-                  <SelectItem value="1">一般會議記錄（非常詳細）</SelectItem>
-                  <SelectItem value="2">上課內容記錄</SelectItem>
-                  <SelectItem value="3">上課內容記錄（非常詳細）</SelectItem>
-                  <SelectItem value="4">專案會議記錄</SelectItem>
-                  <SelectItem value="5">通話記錄</SelectItem>
-                  <SelectItem value="6">合約會議記錄</SelectItem>
-                </SelectContent>
-              </Select>
-            </div>
-          </div>
-          <div className="bg-foreground h-11 rounded-md mt-4 flex items-center gap-2 px-7">
-            <p className="text-white cursor-pointer">
-              <Underline lineColor="bg-white">展開紀錄</Underline>
-            </p>
-            <div className="flex-grow" />
-            <div>
-              <input
-                type="text"
-                placeholder="搜索會議..."
-                className="bg-black text-white rounded-sm px-2 py-1 text-sm"
-              />
-            </div>
-          </div>
-        </div>
-      </div>
     </>
   );
 }
