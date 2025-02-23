8a82a9c96078c500f1aa1f8d88d27a3b52327384
OnCloud <anonymousaaaa41414141@gmail.com>
2025-02-23 23:28:53 +0800

    feat: add preload option to shadcn slider

--- src/components/ui/slider.tsx
+++ src/components/ui/slider.tsx
@@ -5,10 +5,16 @@ import * as SliderPrimitive from "@radix-ui/react-slider";
 
 import { cn } from "@/lib/utils";
 
+type SliderProps = React.ComponentPropsWithoutRef<
+  typeof SliderPrimitive.Root
+> & {
+  preloadValue?: number;
+};
+
 const Slider = React.forwardRef<
   React.ElementRef<typeof SliderPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof SliderPrimitive.Root>
->(({ className, ...props }, ref) => (
+  SliderProps
+>(({ className, preloadValue, ...props }, ref) => (
   <SliderPrimitive.Root
     ref={ref}
     className={cn(
@@ -18,6 +24,12 @@ const Slider = React.forwardRef<
     {...props}
   >
     <SliderPrimitive.Track className="relative h-1.5 w-full grow overflow-hidden rounded-full bg-primary/20 cursor-pointer">
+      {preloadValue !== undefined && (
+        <div
+          className="absolute h-full rounded-full bg-primary/30"
+          style={{ width: `${preloadValue * 100}%` }}
+        />
+      )}
       <SliderPrimitive.Range className="absolute h-full bg-primary" />
     </SliderPrimitive.Track>
     <SliderPrimitive.Thumb className="block h-4 w-4 rounded-full border border-primary/50 bg-background shadow transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 cursor-pointer" />
