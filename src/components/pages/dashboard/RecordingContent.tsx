8136886c2c10d33a29302cb45c69f750a49e5b83
OnCloud <anonymousaaaa41414141@gmail.com>
2025-02-25 01:28:29 +0800

    feat: new transcript ui

--- src/components/pages/dashboard/RecordingContent.tsx
+++ src/components/pages/dashboard/RecordingContent.tsx
@@ -1,15 +1,17 @@
 import { motion } from "motion/react";
-import { createRef } from "react";
+import { createRef, useState } from "react";
 import Markdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { oneDark } from "react-syntax-highlighter/dist/esm/styles/prism";
+import { DotIcon } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
-import { cn } from "@/lib/utils";
 import { AudioPlayer } from "@/components/AudioPlayer";
 import { Recording } from "@/components/pages/dashboard/types/Recording";
 import { Separator } from "@/components/ui/separator";
+import { cn } from "@/lib/utils";
+import { readableTime } from "@/modules/readableTime";
 
 export function RecordingContent({
   recordings,
@@ -27,59 +29,101 @@ export function RecordingContent({
           selectedRecordingId && "w-full h-full",
         )}
       >
+        <div className="flex flex-col gap-5 w-full h-full p-7">
+          <Content
+            selectedRecordingId={selectedRecordingId}
+            recordings={recordings}
+          />
+        </div>
+      </div>
+    </>
+  );
+}
+
+function Content({
+  selectedRecordingId,
+  recordings,
+}: {
+  selectedRecordingId: string;
+  recordings: Recording[];
+}) {
+  const [selectedContentType, setSelectedContentType] = useState<
+    "notes" | "transcript"
+  >("transcript");
+  const syntaxHighlighterRef = createRef<SyntaxHighlighter>();
+
+  const selectedRecording = recordings.find(
+    (recording) => recording.id === selectedRecordingId,
+  );
+
+  if (!selectedRecording) {
+    return <h1>請選擇一個會議記錄</h1>;
+  }
+
+  return (
+    <>
+      <div>
+        <h1 className="text-3xl font-semibold">{selectedRecording.title}</h1>
+        <p className="mt-2 flex items-center text-xs text-muted-foreground">
+          {readableTime.formatElapsedTime(selectedRecording.durationMs).string}
+          <DotIcon className="-mx-1.5 translate-y-[1px]" />
+          {new Date(selectedRecording.createdAt).toLocaleDateString()}
+        </p>
+      </div>
+      <div className="flex gap-3 xl:gap-6 flex-col xl:flex-row justify-between">
+        <AudioPlayer
+          audioSourcePath={selectedRecording.audioSourcePath}
+          className="max-w-sm w-full"
+        />
+        <div className="flex gap-3">
+          <Button className="rounded-full">下載錄音</Button>
+          <Button className="rounded-full">下載會議整理</Button>
+          <div className="p-1 flex gap-1 bg-background rounded-full">
+            <Button
+              variant="ghost"
+              className={cn(
+                "rounded-full p-0 w-20 h-full",
+                selectedContentType === "notes" && "!bg-white",
+              )}
+              onClick={() => setSelectedContentType("notes")}
+            >
+              會議紀錄
+            </Button>
+            <Button
+              variant="ghost"
+              className={cn(
+                "rounded-full p-0 w-20 h-full",
+                selectedContentType === "transcript" && "!bg-white",
+              )}
+              onClick={() => setSelectedContentType("transcript")}
+            >
+              逐字稿
+            </Button>
+          </div>
+        </div>
+      </div>
+      <Separator />
+      <div className="flex-grow relative [&_a]:text-blue-500 max-w-full overflow-y-auto overflow-x-hidden light-scrollbar">
         <motion.div
-          animate={selectedRecordingId ? "open" : "closed"}
+          animate={selectedContentType === "notes" ? "open" : "closed"}
           variants={{
             open: {
               opacity: 1,
+              display: "block",
               x: "0%",
             },
             closed: {
               opacity: 0,
+              display: "none",
               x: "-100%",
             },
           }}
-          className="flex flex-col gap-5 w-full h-full p-7"
+          transition={{
+            x: { type: "spring", bounce: 0, duration: 0.3 },
+            opacity: { duration: 0.3 },
+          }}
+          className="absolute top-0 left-0 bottom-0 right-1.5 prose"
         >
-          <Content selectedRecordingId={selectedRecordingId} />
-        </motion.div>
-      </div>
-    </>
-  );
-
-  function Content({
-    selectedRecordingId,
-  }: {
-    selectedRecordingId: string;
-  }) {
-    const syntaxHighlighterRef = createRef<SyntaxHighlighter>();
-
-    const selectedRecording = recordings.find(
-      (recording) => recording.id === selectedRecordingId,
-    );
-
-    if (!selectedRecording) {
-      return <h1>請選擇一個會議記錄</h1>;
-    }
-
-    return (
-      <>
-        <h1 className="text-3xl font-semibold">{selectedRecording.title}</h1>
-        <div className="flex gap-3 xl:gap-6 flex-col xl:flex-row justify-between">
-          <AudioPlayer
-            audioSourcePath={selectedRecording.audioSourcePath}
-            className="max-w-sm w-full"
-          />
-          <div className="flex gap-3">
-            <Button className="rounded-full">下載錄音</Button>
-            <Button className="rounded-full">下載會議整理</Button>
-            <Button variant="border" className="rounded-full">
-              查看逐字稿
-            </Button>
-          </div>
-        </div>
-        <Separator />
-        <div className="[&_a]:text-blue-500 prose max-w-full overflow-y-auto light-scrollbar pr-3">
           <Markdown
             components={{
               code(props) {
@@ -106,8 +150,45 @@ export function RecordingContent({
           >
             {selectedRecording.notes}
           </Markdown>
-        </div>
-      </>
-    );
-  }
+        </motion.div>
+        <motion.div
+          animate={selectedContentType === "transcript" ? "open" : "closed"}
+          variants={{
+            open: {
+              opacity: 1,
+              display: "block",
+              x: "0%",
+            },
+            closed: {
+              opacity: 0,
+              display: "none",
+              x: "100%",
+            },
+          }}
+          transition={{
+            x: { type: "spring", bounce: 0, duration: 0.3 },
+            opacity: { duration: 0.3 },
+          }}
+          className="absolute top-0 left-0 bottom-0 right-1.5"
+        >
+          <div className="flex flex-col gap-3">
+            {selectedRecording.transcripts.map((transcript, index) => (
+              <div key={index} className="px-4 py-4 bg-stone-100 rounded-lg">
+                <p className="text-sm flex items-center">
+                  <span className="font-semibold">{transcript.userName}</span>
+                  <span className="ml-2 text-muted-foreground">
+                    {
+                      readableTime.formatElapsedTime(transcript.startTime)
+                        .string
+                    }
+                  </span>
+                </p>
+                <p>{transcript.text}</p>
+              </div>
+            ))}
+          </div>
+        </motion.div>
+      </div>
+    </>
+  );
 }
